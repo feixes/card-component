5438524f711af327e803bf3b84ee3ff776286740
Xavier <xavierfeixesguell@gmail.com>
2022-04-13 10:26:18 +0200

    Loading spinner

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
-import Card from './components/Card'
-import GlobalStyles from './global-styles';
-import styled, { ThemeProvider } from 'styled-components'
-import { useState } from 'react'
+import Loading from './components/Loading'
+import styled from 'styled-components'
 
-const BaseTheme = {
-  color: '#222',
-  background: '#f2f4f8'
-}
-const DarkTheme = {
-  color: '#fff',
-  background: '#222'
-}
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  padding: 1.5rem;
-  color: ${(props) => props.theme.color};
-  background: ${(props) => props.theme.background};
-`
+
+
 
 function App() {
 
-  const [baseTheme, setBaseTheme] = useState(true)
 
-  const toggleTheme = () => {
-    setBaseTheme(!baseTheme)
-  }
+
 
   return (
-    <ThemeProvider theme={baseTheme ? BaseTheme : DarkTheme}>
-      <GlobalStyles />
-      <Container>
-        <Card />
-        <button className='btn' onClick={toggleTheme}>toggle theme</button>
-      </Container>
-    </ThemeProvider >
+    <Loading />
   );
 }
 
---
+++ src/Theme-Card.js
@@ -0,0 +1,42 @@
+import Card from './components/Card'
+
+import styled, { ThemeProvider } from 'styled-components'
+
+
+const BaseTheme = {
+    color: '#222',
+    background: '#f2f4f8'
+}
+const DarkTheme = {
+    color: '#fff',
+    background: '#222'
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  padding: 1.5rem;
+  color: ${(props) => props.theme.color};
+  background: ${(props) => props.theme.background};
+`
+
+function App() {
+
+    const [baseTheme, setBaseTheme] = useState(true)
+
+    const toggleTheme = () => {
+        setBaseTheme(!baseTheme)
+    }
+
+    return (
+        <ThemeProvider theme={baseTheme ? BaseTheme : DarkTheme}>
+            <GlobalStyles />
+            <Container>
+                <Card />
+                <button className='btn' onClick={toggleTheme}>toggle theme</button>
+            </Container>
+        </ThemeProvider >
+    );
+}
+
+export default App;
